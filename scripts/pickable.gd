1673d8aa2adfe9c85197e284494b980ce44525ad
Andy <brechtelandreas@gmail.com>
2024-01-28 11:13:55 +0100

    puncake + sounds

--- scripts/pickable.gd
+++ scripts/pickable.gd
@@ -12,6 +12,11 @@ func _ready():
 
 func _on_Pickable_body_entered(body):
 	if body is Player:
+		if (type == 'puncake'):
+			GlobalSignals.emit_signal("kill_all_item_colleced")
+			queue_free()
+			return
+			
 		GlobalSignals.emit_signal("player_got_pickable", type)
 		queue_free()
 
